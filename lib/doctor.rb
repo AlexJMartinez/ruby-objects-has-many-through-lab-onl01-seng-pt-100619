10e855432f26fc014cc4a7fee106af35339000c1
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-14 08:43:35 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -15,7 +15,7 @@ class Doctor
 
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
-    binding.pry
+    #binding.pry
   end
 
   def new_appointment(date, patient)
