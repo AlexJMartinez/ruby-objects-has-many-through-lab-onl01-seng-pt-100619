7c8aa2ea3f88278582f18fbb12afbf8db9f39537
Alex Martinez <alexandr3martin3z@gmail.com>
2019-11-14 08:45:42 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -27,3 +27,14 @@ class Doctor
   end
 
 end
+
+
+
+
+
+#[1] pry(#<Patient>)> Appointment.all.select {|appointment| appointment.patient == self}
+#= self}(#<Patient>)> Appointment.all.select {|appointment| appointment.patient =
+#=> [#<Appointment:0x0000000001b85ff0
+#  @date=#<Doctor:0x0000000001b86090 @name="The Doctor">,
+#  @doctor="Friday, January 32nd",
+#  @patient=#<Patient:0x0000000001b86040 @name="Devin Townsend">>]
